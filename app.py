e718e2865350c817af6010c06678f3a4f9bb67cd
arjunkhangjrakpam <yooarjun@gmail.com>
2021-01-29 06:26:40 +0530

    working directory changed

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pandas as pd
 from flask import Flask, request, render_template
 import pickle
 import os
-os.chdir("/home/fyiacademy/ARJUN/SELF LEARNING/ML/VARIOUS_PYTHON_TASKS/ENTIRE ML PIPELINE with Loan prediction example/CLASSIFICATION")
-#os.chdir(os.getcwd())
+#os.chdir("/home/fyiacademy/ARJUN/SELF LEARNING/ML/VARIOUS_PYTHON_TASKS/ENTIRE ML PIPELINE with Loan prediction example/CLASSIFICATION")
+os.chdir(os.getcwd())
 
 app = Flask(__name__)
 model = pickle.load(open('iris_data_classification.pickle', 'rb'))
